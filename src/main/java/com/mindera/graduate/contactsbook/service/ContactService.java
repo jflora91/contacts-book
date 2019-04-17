ebea4ceab1116288baffdd4bf2a3ba48e55a6218
João Flora <joao.flora@mindera.com>
2019-04-17 16:42:01 +0100

    clean imports declaration

--- src/main/java/com/mindera/graduate/contactsbook/service/ContactService.java
+++ src/main/java/com/mindera/graduate/contactsbook/service/ContactService.java
@@ -15,7 +15,6 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
